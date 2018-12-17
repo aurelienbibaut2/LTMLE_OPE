fb9c8fc788afe26799843dffb949afa6a85d46b5
Aurelien Bibaut <aurelien.bibaut@gmail.com>
2018-12-17 11:21:39 -0500

    Complete implementation of LTMLE

--- R/Experiments.R
+++ R/Experiments.R
@@ -104,7 +104,6 @@ logit <- function(x) { log(x / (1 - x)) }
 
 # LTMLE -------------------------------------------------------------------
 # Generate dataset
-n <- 7
 generate_discrete_MDP_trajectory_with_pi_a_pi_b <- function(s0, state_transition_matrix,
                                                              behavior_action_matrix,
                                                              transition_based_rewards,
@@ -118,25 +117,35 @@ generate_discrete_MDP_trajectory_with_pi_a_pi_b <- function(s0, state_transition
   rho_t <- cumprod(pi_a/pi_b)
   cbind(H, pi_a=pi_a, pi_b=pi_b, rho_t=rho_t)
 }
-n <- 7
+
+# LTMLE on an example
+n <- 10000
 D <- aperm(replicate(n, generate_discrete_MDP_trajectory_with_pi_a_pi_b(1, state_transition_matrix,
                                                          behavior_action_matrix,
                                                          transition_based_rewards,
                                                          horizon)), c(3,1,2))
-V <- rep(0, n)
+epsilons <- c()
+V_evaluated <- rep(0, n) #V_{t+1}(S_{t+1})
 for(t in horizon:1){
   Delta_t <- horizon + 1 - t
-  V_tilde <- (V + Delta_t) / (2 * Delta_t)
-  R_tilde <- (D[, t, 'r'] + Delta_t) / (2 * Delta_t)
-  U_tilde <- R_tilde + V_tilde
-  Q_t <- apply(D[ , t, ], 1, function(x) Q0[t, x['s'], x['a']])
-  Q_tilde_t <- (Q_t + Delta_t) / (2 * Delta_t)
-  epsilon <- glm(U_tilde ~ offset(logit(Q_tilde_t)) + 1, 
-                 family=binomial, weights = D[,t, 'rho_t'])
-  
+  R <- D[, t, 'r'] # R_t
+  U_tilde <- (R + V_evaluated + Delta_t) / (2 * Delta_t) # U_tilde = R_tilde_t + V_tilde_{t+1}(S_t) in the notations of the write-up
+  Q_t_evaluated <- apply(D[ , t, ], 1, function(x) Q0[t, x['s'], x['a']]) # Q_t(A_t, S_t)
+  Q_tilde_t_evaluated <- (Q_t_evaluated + Delta_t) / (2 * Delta_t)
+  epsilon <- glm(U_tilde ~ offset(logit(Q_tilde_t_evaluated)) + 1, 
+                 family=quasibinomial, weights = D[,t, 'rho_t'])$coefficients[1]
+  epsilons <- c(epsilons, epsilon)
   # Evaluate Q_tilde(s_t, a) for a_t = 1, a_t = 2
+  Q_tilde_t_star <- expit(logit((Q0[t, ,] + Delta_t) / (2 * Delta_t)) + epsilon) # Q_tilde_t^*
   # Then set V_tilde(s_t) = \sum_{a} Q_tilde(s_t, a) pi_a(a|s_t)
+  V_tilde <- apply(Q_tilde_t_star * evaluation_action_matrix, 1, sum)
   # Compute V = 2 * Delta_t * (V_tilde - 1)
-  # V is gonna be V_{t+1} in the next iteration
+  V <- 2 * Delta_t * (V_tilde - 1/2)
+  # Evaluate V
+  V_evaluated <-  V[D[, t, 's']]
+  # V_tilde is gonna be V_{t+1} in the next iteration
 }
-# The last V is the LTML estimator of the value
+# The average of the last V is the LTML estimator of the value
+V_hat_LTMLE <- mean(V_evaluated)
+#print(epsilons)
+cat('V_hat_LTMLE: ', V_hat_LTMLE, '\n')
