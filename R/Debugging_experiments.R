33f0e270dec48b1a4db631e900650b63ff7e3241
Wilson Cai <wcai@berkeley.edu>
2018-12-26 12:20:04 -0800

    clean python code

--- R/Debugging_experiments.R
+++ R/Debugging_experiments.R
@@ -17,15 +17,15 @@ D <- generate_discrete_MDP_dataset(n, 1, state_transition_matrix,
 # Save D, V0 and Q0
 D_flattened <- array(D, dim=c(dim(D)[1] * dim(D)[2], dim(D)[3]) )
 Q_hat_flattened <- array(Q_hat, dim=c(dim(Q_hat)[1] * dim(Q_hat)[2], dim(Q_hat)[3]) )
-write.matrix(D_flattened, 'data.csv', sep=',')
-write.matrix(Q_hat_flattened, 'Q_hat.csv', sep=',')
-write.matrix(V_hat, 'V_hat.csv', sep=',')
+MASS::write.matrix(D_flattened, 'data.csv', sep=',')
+MASS::write.matrix(Q_hat_flattened, 'Q_hat.csv', sep=',')
+MASS::write.matrix(V_hat, 'V_hat.csv', sep=',')
 
 cat('True V0: ', V0[1, 1], '\n')
 cat('IS: ', IS_estimator(D), '\n')
 cat('WIS: ', WIS_estimator(D), '\n')
 cat('stepIS: ', stepIS_estimator(D), '\n')
-cat('stepWIS: ', stepWIS_estimator(D), '\n')
+cat('stepWIS: ', stepWIS_estimator(D, horizon), '\n')
 cat('DR: ', DR_estimator_TB(D, Q_hat=Q0, V_hat=V0), '\n')
 cat('LTMLE: ', LTMLE_estimator(D, Q_hat=Q0, V_hat=V0), '\n')
 
