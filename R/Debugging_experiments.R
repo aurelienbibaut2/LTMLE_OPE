d4ef16773ba0c49e9672c45e8ea6e792407d1aba
Aurelien Bibaut <aurelien.bibaut@gmail.com>
2018-12-27 23:36:09 -0500

    Some experimentation

--- R/Debugging_experiments.R
+++ R/Debugging_experiments.R
@@ -4,7 +4,7 @@ source('Estimators.R')
 
 # Debugging experiments ---------------------------------------------------
 # set.seed(1)
-horizon <- 20; n <- 1e3; gamma <- 0.9
+horizon <- 100; n <- 100; gamma <- 0.8
 V0_and_Q0 <- compute_true_V_and_Q(state_transition_matrix,
                                   transition_based_rewards,
                                   evaluation_action_matrix, horizon, gamma = gamma)
@@ -28,9 +28,9 @@ cat('True V0: ', V0[1, 1], '\n')
 # cat('stepIS: ', stepIS_estimator(D), '\n')
 # cat('stepWIS: ', stepWIS_estimator(D, horizon), '\n')
 # cat('DR: ', DR_estimator_TB(D, Q_hat=Q0, V_hat=V0), '\n')
-cat('WDR old: ', WDR_estimator_TB_old(D, Q_hat=Q0, V_hat=V0, gamma=gamma, j=horizon), '\n')
-cat('WDR: ', WDR_estimator_TB(D, Q_hat=Q0, V_hat=V0, gamma=gamma, j=horizon)$g_js[horizon+1], '\n')
-cat('LTMLE: ', LTMLE_estimator(D, Q_hat=Q0, V_hat=V0, evaluation_action_matrix=evaluation_action_matrix, gamma=gamma), '\n')
+# cat('WDR old: ', WDR_estimator_TB_old(D, Q_hat=Q0, V_hat=V0, gamma=gamma, j=horizon), '\n')
+# cat('WDR: ', WDR_estimator_TB(D, Q_hat=Q0, V_hat=V0, gamma=gamma, j=horizon)$g_js[horizon+1], '\n')
+# cat('LTMLE: ', LTMLE_estimator(D, Q_hat=Q0, V_hat=V0, evaluation_action_matrix=evaluation_action_matrix, gamma=gamma), '\n')
 
 # cat('g^(5):', WDR_estimator_TB(D, Q_hat=Q0, V_hat=V0, gamma=gamma, j=5)$g_js[6], '\n')
 # cat('WDR(j=5)', WDR_estimator_TB_old(D, Q_hat=Q0, V_hat=V0, gamma=gamma, j=5), '\n')
@@ -51,3 +51,46 @@ cat('LTMLE: ', LTMLE_estimator(D, Q_hat=Q0, V_hat=V0, evaluation_action_matrix=e
 # cat('WDR(j=0)', WDR_estimator_TB_old(D, Q_hat=Q0, V_hat=V0, gamma=gamma, j=0), '\n')
 
 # debug(WDR_estimator_TB)
+
+
+# Q learning of an initial estimator --------------------------------------
+# D_split1 <- D[1:(floor(n/2)), ,]
+# D_split2 <- D[(1+floor(n/2)):n, , ]
+D_split1 <- D; D_split2 <- D
+# Fit Q_function
+cat('Start Bellman iterations\n')
+transitions_dataset <- make_transitions_dataset(D_split1)
+Q_learning_results <- bellman_iterations(transitions_dataset, evaluation_action_matrix, 
+                                         gamma=gamma, n_iterations=30, V0=V0[1, ])
+cat('Done with Bellman iterations\n')
+plot(Q_learning_results$l2_errors)
+# Replicate accross time points the Bellman iterations based Q-function
+# That would be correct under infinite horizon.
+# Here's it's only approximately correct. Better for early time points than late time points.
+Q_hat <- array(0, dim=c(horizon, n_states, n_actions)); V_hat <- array(0, dim=c(horizon, n_states))
+for(t in 1:horizon){
+  Q_hat[t, ,] <-  Q_learning_results$Q_hat
+  V_hat[t, ] <-  Q_learning_results$V_hat
+}
+
+# Run Monte Carlo simulation ----------------------------------------------
+# Some small MSE experiment
+# DR_estimates <- c(); 
+WDR_estimates <- c(); LTMLE_estimates <- c()
+n <- 100; gamma <- 1
+for(i in 1:500){
+  D <- generate_discrete_MDP_dataset(n, 1, state_transition_matrix,
+                                     behavior_action_matrix,
+                                     transition_based_rewards,
+                                     horizon)
+  # DR_estimates <- c(DR_estimates,
+                    # DR_estimator_TB(D, Q_hat=Q_hat, V_hat=Q_hat))
+  WDR_estimates <- c(WDR_estimates,
+                     WDR_estimator_TB(D, Q_hat=Q_hat, V_hat=V_hat, gamma=gamma, j=horizon)$g_js[horizon+1])
+  LTMLE_estimates <- c(LTMLE_estimates,
+                       LTMLE_estimator(D, Q_hat=Q_hat, V_hat=Q_hat, evaluation_action_matrix=evaluation_action_matrix, gamma=gamma))
+}
+
+# cat('DR MSE:', mean( (DR_estimates - V0[1,1])^2 ), '\n')
+cat('WDR MSE:', mean((WDR_estimates - V0[1,1])^2), '\n')
+cat('LTMLE MSE:', mean((LTMLE_estimates - V0[1,1])^2), '\n')
