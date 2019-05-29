d28a343359553e300b38a4ed57242d2d7982f1dd
Ivana <imalenica.stat@gmail.com>
2019-05-29 13:35:55 -0700

    update figures

--- R/Final_Figures.R
+++ R/Final_Figures.R
@@ -2,6 +2,8 @@
 library(grid)
 library(gridExtra)
 library(here)
+library(cowplot)
+library(ggpubr)
 setwd(here("results"))
 
 MSE_table_be_lb<-read.csv("Final/ModelWin_base_estimators_b0=5e-3_nr=71.csv")
@@ -11,27 +13,38 @@ MSE_table_be_hb$estimator<-as.character(MSE_table_be_hb$estimator)
 MSE_table_be_lb$estimator[MSE_table_be_lb$estimator == 'softened LTMLE'] <- 's LTMLE'
 MSE_table_be_hb$estimator[MSE_table_be_hb$estimator == 'softened LTMLE'] <- 's LTMLE'
 
-library(ggplot2)
+#library(ggplot2)
 
 p1 <- ggplot(data=MSE_table_be_lb, aes(x=id, y=log10(n*MSE), color=estimator, shape=estimator)) + geom_line() + 
-  geom_point(size=2.5) + theme_bw() +
+  geom_point(size=2.5) + theme_bw(base_size = 15) +
   ggtitle(paste("(a) ModelWin low bias")) +
-  theme(legend.position="none") + 
-  #ggtitle(paste(env_name,' horizon=', horizon, ', number of draws per point=', nb_repeats,
-  #              '\nbias=', b0, '*rnorm(1), n=', ns[1])) +
-  scale_shape_manual( values=c('WDR'=19, 'softened_WDR'=19, 'ps WDR'=19, 
-                               'partial LTMLE'=15, 's LTMLE'=15, 'ps LTMLE'=15, 'psp LTMLE'=15) )
+  #theme(legend.position="none") + 
+  scale_shape_manual(values=c('WDR'=19, 'softened_WDR'=19, 'ps WDR'=19, 
+                               'partial LTMLE'=15, 's LTMLE'=15, 'ps LTMLE'=15, 'psp LTMLE'=15) ) +
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.2)) 
 
 p2 <- ggplot(data=MSE_table_be_hb, aes(x=id, y=log10(n*MSE), color=estimator, shape=estimator)) + geom_line() + 
-  geom_point(size=2.5) + theme_bw() +
-  ggtitle(paste("(a) ModelWin high bias")) +
-  #ggtitle(paste(env_name,' horizon=', horizon, ', number of draws per point=', nb_repeats,
-  #              '\nbias=', b0, '*rnorm(1), n=', ns[1])) +
+  geom_point(size=2.5) + theme_bw(base_size = 15) +
+  ggtitle(paste("(b) ModelWin high bias")) +
   scale_shape_manual( values=c('WDR'=19, 'softened_WDR'=19, 'ps WDR'=19, 
-                               'partial LTMLE'=15, 's LTMLE'=15, 'ps LTMLE'=15, 'psp LTMLE'=15) )
+                               'partial LTMLE'=15, 's LTMLE'=15, 'ps LTMLE'=15, 'psp LTMLE'=15) ) + 
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.2),
+        legend.title = element_text(colour="black", size=16, face="bold")) 
 
-grid.arrange(p1, p2, nrow = 1)
+#grid.arrange(p1, p2, nrow = 1)
+ggarrange(p1, p2, ncol=2, common.legend = TRUE, legend="right", font.label="bold")
 
+#####################################################
 MSE_table_GW<-read.csv("GridWorld-1e2-1e3-horizon=100b0=0.005-71draws.csv")
 MSE_table_MWlb<-read.csv("ModelWin-1e2-1e4-horizon=10-b0=5e-3-63draws.csv")
 MSE_table_MWhb<-read.csv("ModelWin-1e2-1e4-horizon=10-b0=5e-2-63draws.csv")
@@ -40,7 +53,7 @@ MSE_table_MWhb$estimator<-as.character(MSE_table_MWhb$estimator)
 MSE_table_MWhb$estimator[MSE_table_MWhb$estimator == 'MAGIC_LTMLE'] <- 'RLTMLE 1'
 MSE_table_MWhb$estimator[MSE_table_MWhb$estimator == 'MAGIC_bootstrap'] <- 'RLTMLE 2'
 
-p1<-ggplot(data=MSE_table_GW, aes(x=log10(n), y=log10(n*MSE), color=estimator, shape=estimator)) + 
+p1<-ggplot(data=MSE_table_GW, aes(x=log10(n), y=round(log10(n*MSE),1), color=estimator, shape=estimator)) + 
   scale_shape_manual( values=c('MAGIC'=15, 'MAGIC_full_library'=15, 'MAGIC_bootstrap'=15, 'RLTMLE 1'=15, 'RLTMLE 2'=15,
                                'C-TMLE-sftning'=15, '1step_LTMLE'=15, 'MAGIC_LTMLE'=15, 'partial_LTMLE_1.0'=15, 'partial_LTMLE_0.3'=15,
                                'LTMLE_1.0'=19, 'LTMLE_0.7'=19, 'LTMLE_0.5'=19, 'LTMLE_0.1'=19, 'LTMLE_0.0'=19,
@@ -51,9 +64,19 @@ p1<-ggplot(data=MSE_table_GW, aes(x=log10(n), y=log10(n*MSE), color=estimator, s
                               'WDR'=4)) +
   theme_bw() +
   geom_line(size=1) + 
-  theme(legend.position="none") +
+  #theme(legend.position="none") +
   geom_point(aes(size=estimator)) +
-  ggtitle(paste("(a) GridWorld"))
+  ggtitle(paste("(a) GridWorld")) + 
+  ylab(label = "log10(n*MSE)") + 
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        #axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"),
+        axis.title.x=element_blank()) 
+    #+ xlim(2, 4)
 
 p2<-ggplot(data=MSE_table_MF, aes(x=log10(n), y=log10(n*MSE), color=estimator, shape=estimator)) + 
   scale_shape_manual( values=c('MAGIC'=15, 'MAGIC_full_library'=15, 'MAGIC_bootstrap'=15, 'RLTMLE 1'=15, 'RLTMLE 2'=15,
@@ -65,9 +88,19 @@ p2<-ggplot(data=MSE_table_MF, aes(x=log10(n), y=log10(n*MSE), color=estimator, s
                               'LTMLE_1.0'=4, 'LTMLE_0.7'=4, 'LTMLE_0.5'=4, 'LTMLE_0.1'=4, 'LTMLE_0.0'=4, 
                               'WDR'=4)) +
   theme_bw() +
-  theme(legend.position="none") +
+  #theme(legend.position="none") +
   geom_line(size=1) + geom_point(aes(size=estimator)) +
-  ggtitle(paste("(b) ModelFail"))
+  ggtitle(paste("(b) ModelFail")) + 
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        #axis.title.x = element_text(color="black", size=14, face="bold"),
+        #axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank()) 
+#+ xlim(2, 4)
 
 p3<-ggplot(data=MSE_table_MWlb, aes(x=log10(n), y=log10(n*MSE), color=estimator, shape=estimator)) + 
   scale_shape_manual( values=c('MAGIC'=15, 'MAGIC_full_library'=15, 'MAGIC_bootstrap'=15, 'RLTMLE 1'=15, 'RLTMLE 2'=15,
@@ -80,7 +113,15 @@ p3<-ggplot(data=MSE_table_MWlb, aes(x=log10(n), y=log10(n*MSE), color=estimator,
                               'WDR'=4)) +
   theme_bw() +
   geom_line(size=1) + geom_point(aes(size=estimator)) +
-  ggtitle(paste("(c) ModelWin low bias"))
+  ggtitle(paste("(c) ModelWin low bias")) + 
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold")) 
+#+ xlim(2, 4)
 
 p4<-ggplot(data=MSE_table_MWhb, aes(x=log10(n), y=log10(n*MSE), color=estimator, shape=estimator)) + 
   scale_shape_manual( values=c('MAGIC'=15, 'MAGIC_full_library'=15, 'MAGIC_bootstrap'=15, 'RLTMLE 1'=15, 'RLTMLE 2'=15,
@@ -93,6 +134,17 @@ p4<-ggplot(data=MSE_table_MWhb, aes(x=log10(n), y=log10(n*MSE), color=estimator,
                               'WDR'=4)) +
   theme_bw() +
   geom_line(size=1) + geom_point(aes(size=estimator)) +
-  ggtitle(paste("(d) ModelWin high bias"))
+  ggtitle(paste("(d) ModelWin high bias")) + 
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        #axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"),
+        axis.title.y=element_blank())
+#+ xlim(2, 4)
 
-grid.arrange(p1, p3, p2, p4, nrow = 2)
+#grid.arrange(p1, p3, p2, p4, nrow = 2)
+ggarrange(p1, p2, p3, p4, nrow=2, ncol=2, common.legend = TRUE, legend="right", font.label="bold")
+
